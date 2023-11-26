dec7e6fca29687286ef54e6bc678423fffbd3aae
ravi-surya <ravisuryak8@gmail.com>
2023-11-26 16:18:10 +0530

    school

---
+++ DIAGONAL_SUM_SCHOOL/diagonalsum.cpp
@@ -0,0 +1,38 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int DiagonalSum(vector<vector<int> >& matrix) {
+        // Code here
+        int sum1 = 0;
+        int sum2 = 0;
+
+        for (int i = 0; i < matrix.size(); i++) {
+            sum1 += matrix[i][i];
+            sum2 += matrix[i][matrix.size() - 1 - i];
+        }
+
+        return sum1+sum2;
+    }
+};
+
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		vector<vector<int>> matrix(n, vector<int>(n,0));
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				cin >> matrix[i][j];
+			}
+		}
+		Solution ob;
+		int ans = ob.DiagonalSum(matrix);
+		cout << ans <<'\n';
+	}
+	return 0;
+}
